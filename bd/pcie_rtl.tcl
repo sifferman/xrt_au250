02a2f55ecda1d51602f8e43af9cab04a40d20cad
Ethan Sifferman <ethan@sifferman.dev>
2025-07-11 11:59:36 -0700

    can generate xo

--- bd/pcie_rtl.tcl
+++ bd/pcie_rtl.tcl
@@ -10,8 +10,27 @@ set_property -dict [list \
   CONFIG.CONST_DATA {0xbeefcafe} \
 ] [get_bd_cells axi_const_rd]
 
-make_bd_intf_pins_external  [get_bd_intf_pins axi_const_rd/s_axi]
-create_bd_port -dir I clk_i
-connect_bd_net [get_bd_ports clk_i] [get_bd_pins axi_const_rd/axi_clk]
-create_bd_port -dir I -type rst rst_ni
-connect_bd_net [get_bd_ports rst_ni] [get_bd_pins axi_const_rd/axi_resetn]
+
+make_bd_intf_pins_external [get_bd_intf_pins axi_const_rd/s_axi]
+
+set s_axi_aclk [ create_bd_port -dir I -type clk -freq_hz 300000000 s_axi_aclk ]
+set_property -dict [ list \
+  CONFIG.ASSOCIATED_BUSIF {s_axi} \
+] $s_axi_aclk
+create_bd_port -dir I -type rst s_axi_aresetn
+
+connect_bd_net [get_bd_ports s_axi_aclk] [get_bd_pins axi_const_rd/axi_clk]
+connect_bd_net [get_bd_ports s_axi_aresetn] [get_bd_pins axi_const_rd/axi_resetn]
+
+# Dummy ports for Vitis
+
+create_bd_port -dir O -type intr interrupt
+
+create_bd_intf_port -mode Master -vlnv xilinx.com:interface:aximm_rtl:1.0 m_axi
+
+set m_axi_aclk [ create_bd_port -dir I -type clk -freq_hz 300000000 m_axi_aclk ]
+set_property -dict [ list \
+  CONFIG.ASSOCIATED_BUSIF {m_axi} \
+  CONFIG.ASSOCIATED_RESET {m_axi_aresetn} \
+] $m_axi_aclk
+create_bd_port -dir I -type rst m_axi_aresetn
--- vivado.tcl
+++ vivado.tcl
@@ -2,16 +2,9 @@
 set project_name [lindex $argv 0]
 set bd_tcl_filename [lindex $argv 1]
 
-start_gui
+# start_gui
 
 create_project au250 $project_name/ -part xcu250-figd2104-2L-e
-
-# add board files
-set_param board.repoPaths [concat [get_param board.repoPaths] {
- ../au250_board_files_20200616/1.2
- ../au250_board_files_20200616/1.3
-}]
-set_property BOARD_PART xilinx.com:au250:part0:1.3 [current_project]
 
 # add_files -norecurse {}
 # set_property file_type {Memory File} [get_files -all]
@@ -23,23 +16,29 @@ add_files -norecurse {
 # add_files -fileset constrs_1 -norecurse {}
 # set_property PROCESSING_ORDER EARLY [get_files -of_objects [get_filesets constrs_1]]
 
+# Block Design
 create_bd_design "design_1"
 source ../$bd_tcl_filename
 save_bd_design
 make_wrapper -files [get_files $project_name/au250.srcs/sources_1/bd/design_1/design_1.bd] -top -import
 
-load_features ipintegrator
+# Open IP Packaging Wizard
+ipx::package_project -vendor ucsc.edu -library user -taxonomy /UserIP -import_files -set_current true -root_dir ./ip-$project_name
 
-ipx::package_project -taxonomy /UserIP -module design_1 -import_files -set_current true
+# Package for Vitis
+set_property sdx_kernel true [ipx::current_core]
+set_property sdx_kernel_type rtl [ipx::current_core]
 
-s
+# Package IP
+ipx::save_core [ipx::current_core]
 
-set_property STEPS.SYNTH_DESIGN.ARGS.FLATTEN_HIERARCHY none [get_runs synth_1]
-set_property STEPS.SYNTH_DESIGN.ARGS.DIRECTIVE PerformanceOptimized [get_runs synth_1]
-launch_runs synth_1 -jobs [exec nproc]
-wait_on_run synth_1
+# Generate xo
+package_xo \
+  -force \
+  -xo_path kernel-$project_name/kernel.xo \
+  -kernel_name $project_name \
+  -ctrl_protocol user_managed \
+  -ip_directory ip-$project_name \
+  -kernel_xml ../kernel.xml
 
-launch_runs impl_1 -to_step write_bitstream -jobs [exec nproc]
-wait_on_run impl_1
-
-# exit
+exit
